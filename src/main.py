fe92b75bb0f6c87da8649dd612d26112bab659c3
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2023-10-23 20:29:09 +0200

    Added requirements

---
+++ src/main.py
@@ -0,0 +1,3 @@
+# Obligatorio: Generar una tabla con TODOS los ficheros (recursivamente) del workspace que contenga el nombre del fichero, el peso REAL y la última fecha de modificación.
+# Opcional: Hacer esto en bash
+
